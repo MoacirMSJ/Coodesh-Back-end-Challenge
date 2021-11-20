24613a285c7493aeb379facf3242403b37929e09
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-11-20 15:01:32 -0300

    Adicionada rota para criar usuário

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 import express from 'express'
-
+import userController from './controllers/userController'
 
 const app = express();
 app.use(express.json())
@@ -8,6 +8,10 @@ app.use(express.json())
 const urlMongo = 'mongodb://user:pass@localhost:27017/pharma'
 mongoose.connect(urlMongo)
 
+app.get('/', (req, res) => {
+  res.send({message: 'REST Back-end Challenge 20201209 Running'})
+})
 
+app.post('/user', userController.newUser)
 
 app.listen(8080, () => console.log('Server started port: 8080'))
