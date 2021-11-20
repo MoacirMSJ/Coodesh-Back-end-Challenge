8a1ee0408ebd6ee1e86dcd3f883f36e160de1bc5
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-11-20 20:12:10 -0300

    Adicionado cron job.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import express from 'express'
 import userController from './controllers/userController'
+import cron from 'node-cron'
+import {getUsersFromRadomUser} from './services/conJobUser'
 
 const app = express();
 app.use(express.json())
@@ -17,5 +19,11 @@ app.post('/user', userController.newUser)
 app.get('/users/:userId', userController.getUser)
 app.get('/users', userController.getAllUsers)
 app.delete('/users/:userId', userController.deleteUser)
+
+cron.schedule('* * * * *', async () => {
+  
+  await getUsersFromRadomUser();
+});
+
 
 app.listen(8080, () => console.log('Server started port: 8080'))
