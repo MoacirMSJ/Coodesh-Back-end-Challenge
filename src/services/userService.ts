a8c0b7669616e33cc6751a5cc581cc40791a9905
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-11-21 13:58:26 -0300

    Adicionando try catchs e padronizando retorno nos serviços de usuário

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,39 +1,61 @@
 import { UserModel } from '../models/userSchema'
 import {User} from '../protocols/user'
+import {HttpResponse} from '../protocols/http-helpers'
 
-export const createOne = async (user: User): Promise<User> => {
-  const userData = await UserModel.create(user);
-  return userData;
+export const createOne = async (user: User): Promise<HttpResponse> => {
+  try {
+    const userData = await UserModel.create(user);
+    return { statusCode: 201, data: { message: "Usuário criado!"} };
+  } catch (error) {
+    return { statusCode: 400, data: { message: "Erro ao criar usuário!" } };
+  }
 }
 
-export const getOne =  async (userId: string ) => {
-  const userData = await UserModel.findOne({_id: userId });
-  return userData;
+export const getOne =  async (userId: string ): Promise<HttpResponse> => {
+  try {
+    const userData = await UserModel.findOne({_id: userId });
+    return { statusCode: 200, data: userData };
+  } catch (error) {
+    return { statusCode: 400, data: { message: "Erro ao buscar usuário!" } };
+  }
 }
 
-export const getMany = async () => {
-  const users = await UserModel.find();
-  return users;
+export const getMany = async (): Promise<HttpResponse> => {
+  try {
+    const users = await UserModel.find();
+    return { statusCode: 200, data: users };
+  } catch (error) {
+    return { statusCode: 400, data: { message: "Erro ao buscar usuários!" } };
+  }
 }
 
-export const deleteOne = async (userId: string) => {
-  const users = await UserModel.deleteOne({_id: userId});
-  return users;
+export const deleteOne = async (userId: string): Promise<HttpResponse> => {
+  try {
+    const users = await UserModel.deleteOne({_id: userId});
+    return { statusCode: 200, data: { message: "Usuário deletado!" } };
+  } catch (error) {
+    return { statusCode: 400, data: { message: "Erro ao deletar usuário!" } };
+  }
 }
 
-export const updateOne = async (userId: string, user: User) => {
-  const users = await UserModel.findOneAndUpdate({ _id: userId}, user);
-  return users;
+export const updateOne = async (userId: string, user: User): Promise<HttpResponse> => {
+  try {
+    const users = await UserModel.findOneAndUpdate({ _id: userId}, user);
+    return { statusCode: 200, data: { message: "Usuário atualizado!" } };
+  } catch (error) {
+    return { statusCode: 400, data: { message: "Erro ao atualizar usuário!" } };
+  }
+
 }
 
 export const createMany = async (userList: User[]) => {
   userList.forEach( async (user) => {
-    const result = await UserModel.exists({id: user.id})
+    const result = await UserModel.exists({id: user.id});
     if (!result) {
       await UserModel.create(user);
     }
     else {
-      console.log('usuario já existe: ', user.name)
+      console.log('usuario já existe: ', user.name );
     }
   })
 }
