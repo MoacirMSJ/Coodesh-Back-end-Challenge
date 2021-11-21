4f071d3a7863165aebfb22b3716fabe95be7622d
MoacirMSJ <moacirmascarenha86@gmail.com>
2021-11-21 13:59:03 -0300

    Controller de usuário atualizando retorno

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,30 +3,30 @@ import {Request, Response} from 'express'
 
 const newUser = async (req: Request, res: Response) => {
     const response = await createOne(req.body);
-    return res.status(200).json(response);
+    return res.status(response.statusCode).json(response.data);
 }
 
 const getUser = async (req: Request, res: Response) => {
     const userId = req.params.userId;
     const response = await getOne(userId);
-    return res.status(200).json(response);
+    return res.status(response.statusCode).json(response.data);
 }
 
 const getAllUsers = async (req: Request, res: Response) => {
     const response = await getMany();
-    return res.status(200).json(response);
+    return res.status(response.statusCode).json(response.data);
 }
 
 const deleteUser = async (req: Request, res: Response) => {
     const userId = req.params.userId;
     const response = await deleteOne(userId);
-    return res.status(200).json(response);
+    return res.status(response.statusCode).json(response.data);
 }
 
 const updateUser = async (req: Request, res: Response) => {
     const userId = req.params.userId;
     const response = await updateOne(userId, req.body);
-    return res.status(200).json(response);
+    return res.status(response.statusCode).json(response.data);
 }
 
 export default {newUser, getUser, getAllUsers, deleteUser, updateUser};
